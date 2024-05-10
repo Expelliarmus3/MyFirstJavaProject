372e05cae9846ab0b4670a8290c4c7470f3ee7aa
Expelliarmus3 <115629352+Expelliarmus3@users.noreply.github.com>
2024-05-10 23:09:47 +0530

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
         //program for taking input from users and adding them
+
+        Scanner input = new Scanner(System.in);
         System.out.print("Enter digit1= ");
         int digit1= input.nextInt();
         System.out.print("Enter digit2= ");
