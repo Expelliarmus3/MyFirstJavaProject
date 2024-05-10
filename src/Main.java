3c90e005abbc591c285dd32ce0c13c7a4f4fb217
Expelliarmus3 <115629352+Expelliarmus3@users.noreply.github.com>
2024-05-10 22:35:22 +0530

    Initial commit for this project

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        //program for taking input from users and adding them
+        System.out.print("Enter digit1= ");
+        int digit1= input.nextInt();
+        System.out.print("Enter digit2= ");
+        int digit2= input.nextInt();
+        System.out.println("Your Sum: "+(digit1+digit2));
 
 
     }
